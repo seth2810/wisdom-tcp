7381e9611d221c74472dcbe24287c79acde840c5
Roman Gafurov <gafurov.roma89@gmail.com>
2025-05-09 04:51:19 +0400

    fixup! feature(server): basic quote client/server implementation

--- internal/client/config.go
+++ internal/client/config.go
@@ -5,5 +5,5 @@ import "time"
 type Config struct {
 	ServerHost         string        `envconfig:"SERVER_HOST"`
 	ServerPort         uint16        `envconfig:"SERVER_PORT" default:"8080"`
-	ConnectionDeadline time.Duration `envconfig:"CONNECTION_DEADLINE" default:"30s"`
+	ConnectionDeadline time.Duration `envconfig:"CONNECTION_DEADLINE" default:"5s"`
 }
--- internal/mtp/config.go
+++ internal/mtp/config.go
@@ -1,15 +1,11 @@
 package mtp
 
 var DefaultConfig = Config{
-	TimeCost:      1,
-	Parallelism:   4,
-	MinDifficulty: 4,
-	MaxDifficulty: 16,
+	TimeCost:    1,
+	Parallelism: 4,
 }
 
 type Config struct {
-	TimeCost      uint32 `json:"time_cost"`      // Number of iterations
-	Parallelism   uint8  `json:"parallelism"`    // Number of parallel threads
-	MinDifficulty uint8  `json:"min_difficulty"` // Minimum difficulty level
-	MaxDifficulty uint8  `json:"max_difficulty"` // Maximum difficulty level
+	TimeCost    uint32 `json:"time_cost"`   // Number of iterations
+	Parallelism uint8  `json:"parallelism"` // Number of parallel threads
 }
